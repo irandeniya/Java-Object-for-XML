088eb8cdfc4d3fe1fd17d5be9115b40695fb261e
Ishan Randeniya <time4ish@gmail.com>
2015-05-22 14:34:48 +0530

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,37 @@
+
+
+/**
+ *
+ * @author Ishan
+ */
+public class Customer {
+
+    private Integer idCustomer;
+    private String firstname;
+    private String lastname;
+
+    public void setIdCustomer(Integer idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+
+    public Integer getIdCustomer() {
+        return idCustomer;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+}
